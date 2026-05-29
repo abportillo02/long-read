c541b1478c6674764c1cd23118e7d4dfa68bc855
abportillo02 <abportillo@coh.org>
2026-05-29 15:08:48 -0700

    gemini data

---
+++ scripts/gemini_transfer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#BATCH --job-name=transfer_test
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -N 1-1
+#SBATCH --ntasks=1
+#SBATCH --mem=75GB
+#SBATCH --time=48:00:00
+#SBATCH --output=transfer_%A_%a.log
+
+scp /home/abportillo/github_repo/long-read/data/ gemini-data1:/scratch/abportillo/long-read/data
